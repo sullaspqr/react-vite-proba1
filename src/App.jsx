b2ef71aaaa6aa25731206979769a3d3287806b5e
sullaspqr <sulla.spqr78@gmail.com>
2025-10-08 11:30:10 +0200

    Számláló + counterek + állapotok

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import Hello from './Hello';
 import Hellop from './Hellop';
-import Counter from './Counter';
 function App() {
-
+const [count, setCount] = useState(10);
+const [count2, setCount2] = useState(20);
   return (
     <div>
-      <Hello name="Barba" age={15} />
-      <Hellop name="Borba" age={18} />
-        <Counter />
+      <Hello name="Péter" age={count} />
+      <Hellop name="Zsuzsa" age={count2} />
+    <div>Péter állapota: {count+1} &nbsp;&nbsp;&nbsp;
+      <button className="btn btn-primary" onClick={() => setCount(count + 1)}>Növelés</button> &nbsp;&nbsp;&nbsp;
+      <button className="btn btn-primary" onClick={() => setCount(count - 1)}>Csökkentés</button> 
+    </div><br />
+    <div>Zsuzsa állapota: {count2} &nbsp;&nbsp;&nbsp;
+      <button className="btn btn-primary" onClick={() => setCount2(count2 + 1)}>Növelés</button> &nbsp;&nbsp;&nbsp;
+      <button className="btn btn-primary" onClick={() => setCount2(count2 - 1)}>Csökkentés</button> 
+    </div>
     </div>
   )
 }
